08783eea5b01f4eb0bb0a862937c17e766e4181d
excaliburne <jeremy.faret@gmail.com>
2020-02-13 23:28:04 +0200

    POM for TestRegisterValid

--- TestRegisterValid.py
+++ TestRegisterValid.py
@@ -2,6 +2,7 @@ import unittest
 import time
 import json
 import sys
+import pathlib
 from random_generator import randomString, randomSimplePassword, randomComplexPassword
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,6 +14,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from locators.locator import RegistrationPage
+
+# generate some ranom info from random_generator.py
+firstname = randomString(10)
+lastname = randomString(10)
+password = randomSimplePassword()
+email = randomString(7) + "@emx.com"
 
 class TestRegistrationvalid(unittest.TestCase):
   def setUp(self):
@@ -21,44 +29,18 @@ class TestRegistrationvalid(unittest.TestCase):
     self.wait = WebDriverWait(self.driver, 100)
   
   def test_registrationvalid(self):
+    driver = self.driver
     self.driver.get("http://tutorialsninja.com/demo/")
-    self.driver.set_window_size(1680, 1027)
-    
-    self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".dropdown .hidden-xs")))
-    self.driver.find_element(By.CSS_SELECTOR, ".dropdown .hidden-xs").click()
+    self.driver.set_window_size(1680, 1027) 
 
-    self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),\'Register\')]")))
-    self.driver.find_element(By.XPATH, "//a[contains(text(),\'Register\')]").click()
+    # going to locators/locator.py to opening menu function
+    open_menu_register = RegistrationPage(driver)
+    open_menu_register.MenuRegister()
 
-    self.wait.until(EC.element_to_be_clickable((By.ID, "input-firstname")))
-    self.driver.find_element(By.ID, "input-firstname").click()
-
-    self.wait.until(EC.element_to_be_clickable((By.ID, "input-firstname")))
-    self.driver.find_element(By.ID, "input-firstname").send_keys(randomString(10))
-
-    self.wait.until(EC.element_to_be_clickable((By.ID, "input-lastname")))
-    self.driver.find_element(By.ID, "input-lastname").click()
-    self.driver.find_element(By.ID, "input-lastname").send_keys(randomString(10))
-
-    self.wait.until(EC.element_to_be_clickable((By.ID, "input-email")))
-    self.driver.find_element(By.ID, "input-email").click()
-    self.driver.find_element(By.ID, "input-email").send_keys("anything@gmail.com")
-
-    self.wait.until(EC.element_to_be_clickable((By.ID, "input-telephone")))
-    self.driver.find_element(By.ID, "input-telephone").click()
-    self.driver.find_element(By.ID, "input-telephone").send_keys("54534264")
-
-    self.wait.until(EC.element_to_be_clickable((By.ID, "input-password")))
-    self.driver.find_element(By.ID, "input-password").click()
-    r_pass = randomComplexPassword()
-    self.driver.find_element(By.ID, "input-password").send_keys(r_pass)
-    self.driver.find_element(By.ID, "input-confirm").click()
-    self.driver.find_element(By.ID, "input-confirm").send_keys(r_pass)
-    self.driver.find_element(By.NAME, "agree").click()
-    time.sleep(1)
-    self.driver.find_element(By.XPATH, "//input[@value=\'Continue\']").click()
-    time.sleep(1)
-
+    # going to locators/locator.py to go trough registration function
+    reg = RegistrationPage(driver)
+    reg.Register(firstname, lastname, email, password)
+    time.sleep(2)
 
   def teardown_method(self):
     self.driver.quit()
--- locators/locator.py
+++ locators/locator.py
@@ -1,1 +1,54 @@
 ### locators soon to be implemented
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+class RegistrationPage():
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 100)
+
+    def MenuRegister(self):
+        # simply opens the drop menu and select "Register"
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".dropdown .hidden-xs")))
+        self.driver.find_element(By.CSS_SELECTOR, ".dropdown .hidden-xs").click()
+
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),\'Register\')]")))
+        self.driver.find_element(By.XPATH, "//a[contains(text(),\'Register\')]").click()
+    
+    def Register(self, firstname, lastname, email, password):
+        # fills the registration form
+        self.wait.until(EC.element_to_be_clickable((By.ID, "input-firstname")))
+        self.driver.find_element(By.ID, "input-firstname").click()
+
+        self.wait.until(EC.element_to_be_clickable((By.ID, "input-firstname")))
+        self.driver.find_element(By.ID, "input-firstname").send_keys(firstname)
+
+        self.wait.until(EC.element_to_be_clickable((By.ID, "input-lastname")))
+        self.driver.find_element(By.ID, "input-lastname").click()
+        self.driver.find_element(By.ID, "input-lastname").send_keys(lastname)
+
+        self.wait.until(EC.element_to_be_clickable((By.ID, "input-email")))
+        self.driver.find_element(By.ID, "input-email").click()
+        self.driver.find_element(By.ID, "input-email").send_keys(email)
+
+        self.wait.until(EC.element_to_be_clickable((By.ID, "input-telephone")))
+        self.driver.find_element(By.ID, "input-telephone").click()
+        self.driver.find_element(By.ID, "input-telephone").send_keys("54534264")
+
+        self.wait.until(EC.element_to_be_clickable((By.ID, "input-password")))
+        self.driver.find_element(By.ID, "input-password").click()
+        self.driver.find_element(By.ID, "input-password").send_keys(password)
+        self.driver.find_element(By.ID, "input-confirm").click()
+        self.driver.find_element(By.ID, "input-confirm").send_keys(password)
+        self.driver.find_element(By.NAME, "agree").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//input[@value=\'Continue\']").click()
+        time.sleep(1)
+
+
+
+
+
+    
